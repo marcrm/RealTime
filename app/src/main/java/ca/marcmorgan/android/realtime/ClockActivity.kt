28328b6183828e6635b19d78377f8c9e26e230a5
Marc Morgan <mmorgan@marcmorgan.ca>
2018-11-12 23:22:01 -0800

    Calculate integral time zone from location.

--- app/src/main/java/ca/marcmorgan/android/realtime/ClockActivity.kt
+++ app/src/main/java/ca/marcmorgan/android/realtime/ClockActivity.kt
@@ -1,8 +1,13 @@
 package ca.marcmorgan.android.realtime
 
+import android.content.Context
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.widget.TextView
 import org.threeten.bp.LocalTime
 import org.threeten.bp.ZoneId
@@ -12,6 +17,7 @@ class ClockActivity : AppCompatActivity() {
 
     companion object {
         const val UPDATE_MS: Long = 100
+        const val TAG = "ClockActivity"
     }
 
     var utcTimeTextView: TextView? = null
@@ -19,6 +25,16 @@ class ClockActivity : AppCompatActivity() {
     var dstTimeTextView: TextView? = null
     var realTimeTextView: TextView? = null
     val refreshHandler = Handler()
+
+    var location: Location? = null
+    val locationListener = object : LocationListener {
+        override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {}
+        override fun onProviderEnabled(p0: String?) {}
+        override fun onProviderDisabled(p0: String?) {}
+        override fun onLocationChanged(p0: Location?) {
+            location = p0
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,8 +45,18 @@ class ClockActivity : AppCompatActivity() {
         dstTimeTextView = findViewById(R.id.clock_dst_time)
         realTimeTextView = findViewById(R.id.clock_realtime_time)
 
+        initLocation()
         updateTime()
         initTimer()
+    }
+
+    private fun initLocation() {
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager?
+        try {
+            locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0L, 0f, locationListener)
+        } catch (ex: SecurityException) {
+            Log.d(TAG, "location not permitted")
+        }
     }
 
     private fun initTimer() {
@@ -41,15 +67,19 @@ class ClockActivity : AppCompatActivity() {
         refreshHandler.postDelayed(updateRunnable, UPDATE_MS)
     }
 
+    private fun longitudeToSeconds(loc: Location) = (loc.longitude / 180.0) * 12 * 60 * 60
+
     private fun updateTime() {
         val blankTime = getString(R.string.empty_time)
         val format = DateTimeFormatter.ofPattern("h:mm:ss a")
         val utcTime = LocalTime.now(ZoneId.of("UTC"))
+        val integralTime = location?.let {
+            utcTime.plusSeconds(longitudeToSeconds(it).toLong())
+        }
 
         utcTimeTextView?.text = utcTime.format(format)
-
-        for(v in listOf(integralTimeTextView, dstTimeTextView, realTimeTextView)) {
-            v?.text = blankTime
-        }
+        integralTimeTextView?.text = integralTime?.format(format) ?: blankTime
+        dstTimeTextView?.text = blankTime
+        realTimeTextView?.text = blankTime
     }
 }
