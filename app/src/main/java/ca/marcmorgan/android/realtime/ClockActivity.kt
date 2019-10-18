7963052aa0f237916f639644b8ec6a3687d05948
Marc Morgan <mmorgan@marcmorgan.ca>
2019-10-18 15:40:47 -0700

    Update dependencies.

--- app/src/main/java/ca/marcmorgan/android/realtime/ClockActivity.kt
+++ app/src/main/java/ca/marcmorgan/android/realtime/ClockActivity.kt
@@ -3,11 +3,11 @@ package ca.marcmorgan.android.realtime
 import android.Manifest
 import android.content.pm.PackageManager
 import android.location.Location
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import android.util.Log
 import android.widget.TextView
 import com.google.android.gms.location.*
